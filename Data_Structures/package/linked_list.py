417792e4d367e3bfe7665bd39377061d1a4b035b
momoein <100838174+momoein@users.noreply.github.com>
2023-10-30 11:29:05 +0330

    add SllNode and DllNode in linkde_list module

---
+++ Data_Structures/package/linked_list.py
@@ -0,0 +1,11 @@
+
+class SllNode:
+    def __init__(self, element):
+        self.element = element
+        self.next = None
+class DllNode:
+    def __init__(self, element):
+        self.element = element
+        self.next = None
+        self.prev = None
+
