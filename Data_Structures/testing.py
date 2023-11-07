1879f6f458be643ed77ff12c7342991864edb71e
momoein <100838174+momoein@users.noreply.github.com>
2023-11-08 00:47:37 +0330

    change testing file

--- Data_Structures/testing.py
+++ Data_Structures/testing.py
@@ -1,37 +1,24 @@
 from structure.linked_list import SLL
+from tasks.students import *
+
+# def test_add_first():
+#     sll = SLL()
+#     for i in range(10):
+#         sll.add_first(i)
+#     for i in range(10):
+#         assert sll[i] == i, "element %d not found" %i
+
+# # test_add_first()
 
 
-def test_add_first():
-    sll = SLL()
-    for i in range(10):
-        sll.add_first(i)
-    for i in range(10):
-        assert sll[i] == i, "element %d not found" %i
+# def division(x,y):
+#   assert y!=0, "y cannot be zero"
+#   print(x/y)
 
-# test_add_first()
-
-sll = SLL()
-sll.add_first(8)
-sll.add_after(8, 9)
-sll.add_last(10)
-sll.show_all()
-# sll.del_first()
-# sll.del_last()
-# sll.delete(8)
-# sll.show_all()
-try:
-    for i in range(10):
-        print(sll[i])
-except Exception as err:
-    print(f"Error {type(err)}:",err)
-print("everything good")
+# try:
+#   division(10,0)
+# except AssertionError as msg:
+#   print(msg)
 
 
-def division(x,y):
-  assert y!=0, "y cannot be zero"
-  print(x/y)
-
-try:
-  division(10,0)
-except AssertionError as msg:
-  print(msg)
+test_students()
