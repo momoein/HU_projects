10ef62a976ca59e52a29ff0004e2bed85a4d0405
momoein <100838174+momoein@users.noreply.github.com>
2023-11-21 13:23:35 +0330

    add selection sort

--- Data_Structures/toolbox/sort.py
+++ Data_Structures/toolbox/sort.py
@@ -1,3 +1,4 @@
+
 def insertion_sort(array):
     n = len(array)
     key = array[1]
@@ -10,6 +11,7 @@ def insertion_sort(array):
         array[i] = key
     return array
 
+
 def bubble_sort(array):
     for i in range(len(array)):
         j = 0
@@ -20,3 +22,50 @@ def bubble_sort(array):
                 array[j] = temp
             j += 1
     return array
+
+
+def selection_sort(array):
+    n = len(array)
+    for i in range(n):
+        min = i
+        for j in  range(i, n):
+            if array[j] < array[min]:
+                min = j
+        temp = array[i]
+        array[i] = array[min]
+        array[min] = temp
+    return array
+
+# def merge_sort(array): # it have bug
+#     n = len(array)
+#     if n == 1:
+#         return array
+#     m = n // 2
+#     L = array[:m]
+#     R = array[m:]
+
+#     merge_sort(L)
+#     merge_sort(R)
+#     merge(L, R)
+
+# def merge(L, R):
+#     n, m = len(L), len(R)
+#     i, j = 0, 0
+#     array = [None for i in range(n+m)]
+#     for k in range(0, n+m):
+#         if i == n:
+#             i_value = R[j] + 1
+#         else:
+#             i_value = L[i]
+#         if j == m:
+#             j_value = L[i] + 1
+#         else:
+#             j_value = R[j]
+
+#         if i_value <= j_value:
+#             array[k] = i_value
+#             i += 1
+#         if i_value > j_value:
+#             array[k] = j_value
+#             j += 1
+#     return array
