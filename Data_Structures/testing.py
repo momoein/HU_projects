23d40413783224b955b79eb3e6724221537fac56
momoein <100838174+momoein@users.noreply.github.com>
2023-11-21 14:24:24 +0330

    ignore Data_Structures/testing.py

--- Data_Structures/testing.py
+++ Data_Structures/testing.py
@@ -7,32 +7,8 @@ import numpy as np
 from time import time
 
 
-# def division(x,y):
-#   assert y!=0, "y cannot be zero"
-#   print(x/y)
-
-# try:
-#   division(10,0)
-# except AssertionError as msg:
-#   print(msg)
 
 
-# students test
-# st = Students()
-# n = st.create_node(StuNode, 400, "momo")
-# print(n)
 
-# # SLL test
-# sll = SLL()
-# sll.add_first(StuNode, 1, "momo")
-# print(sll.tail.name)
-# print(sll.tail)
-# sll.add_first(StuNode, 2, "mamad")
-# print(sll.head.name)
-# print(sll.head)
-# sll.add_after('stu_code', 3, StuNode, 3, "ali")
-# target, prev = sll.search('name', 'ali')
-# print(target.name)
-# print(prev.name)
 
 
