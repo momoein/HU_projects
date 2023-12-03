b15be934aaf036c02d7f481c3ffc0977266f2b20
momoein <100838174+momoein@users.noreply.github.com>
2023-12-04 01:01:50 +0330

    create function to get power set of a list

---
+++ Data_Structures/tasks/power_set.py
@@ -0,0 +1,19 @@
+a = [1,2,3]
+
+def power_set(array):
+    if len(array) == 1:
+        return array
+    n_th = array[-1]
+    set = power_set(array[:-1])
+    # creat power set for new list
+    new = []
+    for i in set:
+        if type(i) is not list:
+            new.append([i, n_th])
+        else:
+            new.append(i + [n_th])
+    # combine old set and new set
+    set = set + [n_th] + new
+    return set
+
+print(power_set(a))
