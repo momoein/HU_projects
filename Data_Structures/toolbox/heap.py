81bd4c22ced3fe01440b556b1534c389f2104e1b
momoein <100838174+momoein@users.noreply.github.com>
2023-12-25 00:18:13 +0330

    max_heapify(), build_heap(), increase_key() were created

--- Data_Structures/toolbox/heap.py
+++ Data_Structures/toolbox/heap.py
@@ -1,76 +1,49 @@
-import numpy as np
 
+def parent(i):
+    # (i-1) // k
+    return (i-1) // 2
 
-class MaxHeap:
-    def __init__(self, size):
-        self.A = np.zeros(size)
-        self.useful = 0
+def left(i):
+    return 2*i + 1
 
+def right(i):
+    # 2*i + k
+    return 2*i + 2
 
-    def parent(self, i):
-        return i // 2
+def increase_key(array, i, key):
+    p = parent(i)
+    if key < array[p]:
+        array[i] = key
+    else:
+        array[i] = array[p]
+        array[p] = key
     
-    def left_child(self, i):
-        return 2*i
-    
-    def right_child(self, i):
-        return 2*i
-    
-    def increase_key(self, i, key):
-        if self.A[i] > key:
-            print("Error: key smaller then the current key")
-            return
-        self.A[i] = key
-        while i > 1 and self.A[i] > key:
-            temp = self.A[self.parent(i)]
-            self.A[self.parent(i)] = self.A[i]
-            self.A[self.parent(i)] = temp
-            i = self.parent(i)
+    while p > 1:
+        increase_key(array, p, key)
+        p = parent(p)
 
-
-
-
-
-print("--->" ,np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-# A = np.zeros(10)
-A = np.array([0, 3, 6, 1, 9, 7, 18, 9, 18, 5, 8])
-print("run0", A)
 
 def max_heapify(array, i):
-    left = 2*i
-    right = 2*i + 1
-    bigchild = 0
+    l = left(i)
+    r = right(i)
 
-    if left < len(array) and array[left] > array[i]:
-        bigchild = left
+    if l < len(array) and array[l] > array[i]:
+        big_child = l
     else:
-        bigchild = i
-
-    if right < len(array) and array[right] > array[i]:
-        bigchild = right
-
-    if i != bigchild:
-        temp = array[i]
-        array[i] = array[bigchild]
-        array[bigchild] = temp
-        max_heapify(array, bigchild)
-
-
-max_heapify(A, 5)
-print("run5", A)
-max_heapify(A, 4)
-print("run4", A)
-max_heapify(A, 3)
-print("run3", A)
-max_heapify(A, 2)
-print("run2", A)
-max_heapify(A, 1)
-print("run1", A)
-
-
-# # def build_heap(array):
-# #     n = len(array) - 1
-# #     for i in range(n, n//2, -1):
-# #         heapify(array, i)
+        big_child = i
+    
+    if r < len(array) and array[r] > array[big_child]:
+        big_child = r
+    
+    if big_child != i:
+        # swap
+        array[i], array[big_child] = array[big_child], array[i]
+        max_heapify(array, big_child)
+        
+    
+def build_heap(array):
+    size = len(array)
+    for i in range(size//2, -1, -1):
+        max_heapify(array, i)
 
 
