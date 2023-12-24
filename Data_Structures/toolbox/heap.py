39ec413dade00b0eff6030d854bf6136f87c13f2
momoein <100838174+momoein@users.noreply.github.com>
2023-12-25 01:38:47 +0330

    Maxaheap was finished

--- Data_Structures/toolbox/heap.py
+++ Data_Structures/toolbox/heap.py
@@ -1,3 +1,4 @@
+import math
 
 def parent(i):
     # (i-1) // k
@@ -10,35 +11,35 @@ def right(i):
     # 2*i + k
     return 2*i + 2
 
+
 def increase_key(array, i, key):
-    p = parent(i)
-    if key < array[p]:
-        array[i] = key
-    else:
-        array[i] = array[p]
-        array[p] = key
+    if array[i] > key:
+        print("Error: new key is smaller than the current key")
+        return
     
-    while p > 1:
-        increase_key(array, p, key)
-        p = parent(p)
+    array[i] = key
+    while i > 0:
+        if array[i] > array[parent(i)]:
+            # swap
+            array[i], array[parent(i)] = array[parent(i)], array[i]
+            i = parent(i)
 
 
 def max_heapify(array, i):
     l = left(i)
     r = right(i)
+    largest = i
 
     if l < len(array) and array[l] > array[i]:
-        big_child = l
-    else:
-        big_child = i
+        largest = l
     
-    if r < len(array) and array[r] > array[big_child]:
-        big_child = r
+    if r < len(array) and array[r] > array[largest]:
+        largest = r
     
-    if big_child != i:
+    if largest != i:
         # swap
-        array[i], array[big_child] = array[big_child], array[i]
-        max_heapify(array, big_child)
+        array[i], array[largest] = array[largest], array[i]
+        max_heapify(array, largest)
         
     
 def build_heap(array):
@@ -46,4 +47,40 @@ def build_heap(array):
     for i in range(size//2, -1, -1):
         max_heapify(array, i)
 
+
+class MaxHeap:
+    def __init__(self, array=[]):
+        self.array = array
+
+    def insert(self, key):
+        self.array.append(-math.inf)
+        i = len(self.array) - 1
+        increase_key(self.array, i, key)
+
+    def delete(self):
+        self.array[0] = self.array[-1]
+        self.array.pop(-1)
+        self.heapify(0)
+
+    def build(self):
+        build_heap(self.array)
+    
+    def heapify(self, i):
+        max_heapify(self.array, i)
+
+    def show(self):
+        print(self.array)
+
+    
+
+        
+
+a = [1, 2, 3, 4, 5, 6, 7]
+build_heap(a)
+print(a)
+h = MaxHeap(a)
+h.insert(10)
+print(h.array)
+h.delete()
+h.show()
 
