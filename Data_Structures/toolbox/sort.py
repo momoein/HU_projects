b9606650a9ebe6523b256c70e64c8911c7ce703c
momoein <100838174+momoein@users.noreply.github.com>
2023-12-27 18:03:23 +0330

    radix_sort() was added

--- Data_Structures/toolbox/sort.py
+++ Data_Structures/toolbox/sort.py
@@ -107,3 +107,36 @@ def count_sort(array, max):
         c[a[i]] -= 1
 
     return b[1:]
+
+
+
+def radix_sort(array, digits):
+    d = digits
+    for i in range(1, d+1):
+        array = count_sort_by_digit(array, 9, i)
+    return array
+
+
+def count_sort_by_digit(array, max, digit):
+    a, m = array, max
+    c = [0 for i in range(m+1)] 
+    b = [0 for i in range(len(array) + 1)]
+    
+    for i in range(len(a)):
+        c[which_digit(a[i], digit)] += 1
+    
+    for i in range(1, len(c)):
+        c[i] += c[i-1]
+    
+    for i in range(len(a)-1, -1, -1):
+        b[c[which_digit(a[i], digit)]] = a[i]
+        c[which_digit(a[i], digit)] -= 1
+
+    return b[1:]
+
+
+def which_digit(integer, digit):
+    base =  10 ** (digit)
+    integer %= base
+    base /= 10
+    return int(integer // base)
