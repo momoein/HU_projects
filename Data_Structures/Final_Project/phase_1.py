ccd33ce43285d1daac956c6d5ee175d27f056daa
momoein <100838174+momoein@users.noreply.github.com>
2024-01-26 03:44:40 +0330

    phase_1 was finished

--- Data_Structures/Final_Project/phase_1.py
+++ Data_Structures/Final_Project/phase_1.py
@@ -16,17 +16,19 @@ class Consignment:
         self.group = None
 
     def __repr__(self) -> str:
-        info = f"""
-        id: '{self.id}'
-        weight: '{self.weight}'
-        subscription_code: '{self.subscription_code}'
-        registration_date: '{self.registration_date}'
-        delivery_date: '{self.delivery_date}'
-        cost: '{self.cost}'
-        group: '{self.group}'
-        """
+        info = f"""Consignment:
+            id: '{self.id}'
+            weight: '{self.weight}'
+            subscription_code: '{self.subscription_code}'
+            registration_date: '{self.registration_date}'
+            delivery_date: '{self.delivery_date}'
+            cost: '{self.cost}'
+            group: '{self.group}' """
         return info
-        
+
+
+
+
 class Costs:
     def __init__(self) -> None:
         self.__bst = BST()
@@ -52,24 +54,55 @@ class Costs:
     def __get_lt(self, root, cost, sll):
         """insert in to the sll if node.key <= cost"""
         if root is None:
-            return 
+            return None
         else:
             self.__get_lt(root.left, cost, sll)
             #
-            if root.key <= cost:
+            if root.key > cost:
+                return None
+            else:
                 for node in root.data:
                     sll.insert(node.data)
             #
-            if root.right and root.right.key <= cost:
-                self.__get_lt(root.right, cost, sll)
+            self.__get_lt(root.right, cost, sll)
+
 
     def __search(self, key):
         return self.__bst.search(key)
+    
+    ...
 
-class Phase1:
+
+
+class ConsignmentManager:
     def __init__(self) -> None:
         self.__table = DynamicHash()
         self.__costs = Costs()
+
+
+    def add(self, data):
+        node = self.create_consignment_node(data)
+        if node.id not in self.__table:
+            self.__table[node.id] = node
+            self.__costs.add(id=node.id, cost=int(node.cost))
+        else:
+            print(f"tekrari: {node.id}")
+
+    
+    def search(self, id):
+        item = self.__table[id]
+        return item
+    
+    def get_costs_less_then(self, cost: int):
+        for id in self.__costs.less_of(cost):
+            # print(id)
+            print(self.__table[id])
+
+    
+    def show_all(self):
+        for key in self.__table:
+            print(self.__table[key])
+
 
     def create_consignment_node(self, item_values):
         attr = ("id", 
@@ -98,39 +131,16 @@ class Phase1:
     def insert_from_csv(self):
         for item in self.consignments_csv():
             self.add(item)
-            
-    
-    def add(self, data):
-        node = self.create_consignment_node(data)
-        if node.id not in self.__table:
-            self.__table[node.id] = node
-            self.__costs.add(id=node.id, cost=int(node.cost))
 
-    
-    def search(self, id):
-        item = self.__table[id]
-        return item
-    
-    def cost_search(self, cost: int):
-        for id in self.__costs.less_of(cost):
-            print(id)
-            # print(repr(self.__table[id]))
-
-    
-    def show_all(self):
-        for key in self.__table:
-            print(key, ":", self.__table[key])
-    
+    ...
 
 
 
 
 
-
-f1 = Phase1()
-f1.insert_from_csv()
-# f1.show_all()
-# print(f1.search("ABC1234").weight)
-
-f1.cost_search(10000000)
-
+if __name__ == "__main__":
+    c_manager = ConsignmentManager()
+    c_manager.insert_from_csv()
+    # c_manager.show_all()
+    # print(c_manager.search("CDA1234"))
+    # c_manager.get_costs_less_then(1750)
