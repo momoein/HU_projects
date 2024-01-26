14f756f53d2039afb69362a40652ce64c7232ae8
momoein <100838174+momoein@users.noreply.github.com>
2024-01-26 14:57:11 +0330

    create LoadCSV class for file handlling

---
+++ Data_Structures/Final_Project/file_handler.py
@@ -0,0 +1,39 @@
+from typing import Iterable, Any
+import csv
+
+
+
+class LoadCSV:
+
+    def __init__(self, path) -> None:
+        self.path = path
+
+
+    def lines(self):
+        with open(self.path, 'r', newline='') as f:
+            for line in csv.reader(f):
+                yield line
+
+
+    def write(self, lines: Iterable[Iterable[Any]], fields=None):
+        """filds: None | Iterable[Any]"""
+        #
+        with open(self.path, "w")as file:
+            csvwriter = csv.writer(file, lineterminator="\n")
+            if fields:
+                csvwriter.writerow(fields)
+            csvwriter.writerows(lines)
+
+
+    def append(self, lines: Iterable[Iterable[Any]]):
+        #
+        with open(self.path, "a")as file:
+            csvwriter = csv.writer(file, lineterminator="\n")
+            csvwriter.writerows(lines) 
+
+
+    def create(self):
+        with open(self.path, "x")as file:
+            file.close()
+
+
