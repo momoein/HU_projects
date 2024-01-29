f81831d71c313977aa757a09496f54db0c3c22cf
momoein <100838174+momoein@users.noreply.github.com>
2024-01-29 06:18:57 +0330

    creat cli for terminal

---
+++ Data_Structures/Final_Project/cli.py
@@ -0,0 +1,45 @@
+main_cli ="""
+-------------------------------------------
+#  main menu
+
+1) phase 1 
+2) phase 2 
+3) phase 3 
+
+exit >> 00
+"""
+
+phase1_cli ="""
+-------------------------------------------
+#  Consignment manager
+
+1) Add consingment 
+2) Search by code 
+3) All under cost 
+4) Show all 
+5) Insert from csv
+
+back >> 0  
+exit >> 00
+"""
+
+
+phase2_cli ="""
+-------------------------------------------
+#  Delivery manager
+
+ 1) Add delivery agent 
+ 2) Edit delivery agent info 
+ 3) Allocation of parcel 
+ 4) Show all delivery agent 
+ 5) K best delivery agent 
+ 6) Find by agent name 
+ 7) Show waiting queue 
+ 8) Show all parcel 
+ 9) Show all delivered parcel 
+10) Show all not delivered parcel 
+11) show by day 
+
+back >> 0  
+exit >> 00
+"""
