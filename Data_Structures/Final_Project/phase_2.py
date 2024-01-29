cf145a2c69e43f427df9185ca4cdd530cc29aa10
momoein <100838174+momoein@users.noreply.github.com>
2024-01-29 06:20:26 +0330

    base of phase2 was strated

--- Data_Structures/Final_Project/phase_2.py
+++ Data_Structures/Final_Project/phase_2.py
@@ -3,8 +3,8 @@ from tools.hash_table import DynamicHash
 from tools.linked_list import SLL
 from tools.applied_functions import set_attrs
 from file_handler import LoadCSV
+from tools.array import DynamicArray
 
-import random
 
 
 class Paik:
@@ -32,11 +32,12 @@ class Paik:
 
 
 
-class Employees:
+class Deliveries:
 
     def __init__(self) -> None:
         self.__table = DynamicHash()
         self.__nop = 0   #number of paik's
+        self.__deliveries = DynamicArray()
     
 
     def insert_paik_from_csv(self, file_path):
@@ -46,7 +47,7 @@ class Employees:
 
 
     def add_paik(self, paik_info: Iterable[str]):
-        paik = self.__get_paik_node(paik_info)
+        paik = self.__make_paik_node(paik_info)
         n_code = paik.national_code
         # checks the paik national code
         if n_code in self.__table:
@@ -56,10 +57,11 @@ class Employees:
         # Adding paik to the table
         self.__nop += 1
         paik.recruitment_id = str(self.__nop)
+        self.__deliveries.append(paik)
         self.__table[n_code] = paik
 
 
-    def __get_paik_node(self, paik_info: Iterable[str]):
+    def __make_paik_node(self, paik_info: Iterable[str]):
         attrs = ("name", 
                  "last_name", 
                  "national_code", 
@@ -71,52 +73,122 @@ class Employees:
 
 
     def show_all_paikes(self):
-        for key in self.__table:
-            print(self.__table[key])
+        for delivery in self.__deliveries:
+            print(delivery)
 
+
+    def get_paik_by_id(self, id):
+        return self.__deliveries[int(id)-1]
+    
+
+    def edit_paik_info(self, id, keyword, value):
+        if keyword == "recruitment_id":
+            error = "`recruitment id` has not changeable"
+            print(error)
+            return None
+        paik = self.get_paik_by_id(id)
+        setattr(paik, keyword, value)
+        #
+        print(paik)
+        print(f"The '{keyword}' change to:", getattr(paik, keyword))
     ...
 
 
 
 
+class NotDeliveredParcel:
+    def __init__(self) -> None:
+        self.parcel_id = None
+        self.weight = None
+        self.subscription_code = None
+        self.registration_date = None
+        self.cost = None
+        self.group = None
 
-em = Employees()
-path = "data_structures\\Final_Project\\data\\delivery_information.csv"
-em.insert_paik_from_csv(path)
-em.show_all_paikes()
+    def __repr__(self) -> str:
+        info = f"""not delivered parcel:
+            id: '{self.id}'
+            weight: '{self.weight}'
+            subscription_code: '{self.subscription_code}'
+            registration_date: '{self.registration_date}'
+            cost: '{self.cost}'
+            group: '{self.group}' """
+        return info
 
 
+class SendQueue:
 
+    def __init__(self) -> None:
+        self.__days = DynamicHash()
 
-def make_tests_paik(num=10):
-    tf_path = "data_structures\\Final_Project\\data\\test.csv"
-    tf = LoadCSV(tf_path)
+    def insert_from_csv(self, path):
+        file = LoadCSV(path)
+        for line in file.lines():
+            self.add(line)
 
-    tf_filds = ["name","last_name","national_code","capacity","status"]
-    rows = []
-    chars = [chr(c) for c in range(97, 123)]
+    def add(self, parcel_info):
+        parcel = self.__make_parcel_node(parcel_info)
+        ...
+        
 
-    for _ in range(num):
-        row = []
-        random.shuffle(chars)
-        name = "".join(random.choices(population=chars, k=5))
-        row.append(name)
-        random.shuffle(chars)
-        last_name = "".join(random.choices(population=chars, k=5))
-        row.append(last_name)
-        ncode = str(random.randint(1_000_000_000, 9_999_999_999))
-        row.append(ncode)
-        capa = str(random.randint(10, 99))
-        row.append(capa)
-        stat = random.choice(["A", "D"])
-        row.append(stat)
-        #
-        rows.append(row)
+    def __make_parcel_node(self, parcel_info: Iterable[str]):
+        attrs = ("id", 
+                "weight", 
+                "subscription_code", 
+                "registration_date", 
+                "cost", 
+                "group",)
+        parcel = NotDeliveredParcel()
+        set_attrs(parcel, attrs, parcel_info)
+        return parcel
 
-    if tf.is_exist():
-        tf.append(rows)
-    else:
-        tf.write(rows, tf_filds)
+    
+    
+class Day:
+    def __init__(self) -> None:
+        self.waiting = DynamicHash()
+        self.Posted = SLL()
+        self.failed = SLL()
+
+    # def add(self, percel_node):
+    #     ...
+
+class Days:
+    def __init__(self) -> None:
+        self.__table = DynamicHash()
+
+    
     
 
-# make_tests_paik()
+
+from datetime import date
+
+
+
+def get_date(date_: str):
+    D = date_.split("-")
+    y, m, d = int(D[0]), int(D[1]), int(D[2])
+    return date(y, m, d)
+
+def days_difference(d1, d2):
+    delta = d2 - d1
+    return delta.days
+
+start = get_date("2024-01-05")
+end = get_date("2024-01-07")
+
+res = days_difference(end, start)
+print(res)
+
+
+
+
+
+# em = Deliveries()
+# path = "data_structures\\Final_Project\\data\\delivery_information.csv"
+# em.insert_paik_from_csv(path)
+# em.show_all_paikes()
+# print(em.get_paik_by_id(10))
+# em.edit_paik_info(10, "capacity", "85")
+
+
