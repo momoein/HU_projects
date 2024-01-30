9c5142bfe2856dba8891eb66422e8b755464903d
momoein <100838174+momoein@users.noreply.github.com>
2024-01-31 03:13:35 +0330

    input controller was created

---
+++ Data_Structures/Final_Project/interface/input_controller.py
@@ -0,0 +1,52 @@
+from tools.array import Array
+
+
+class InputController:
+    
+    @staticmethod
+    def get_parcel_id():
+        inp = input(">>> ").strip()
+        while True:
+            if len(inp) == 7 and inp[:3].isalpha() and inp[3:].isdigit():
+                up = inp[:3].upper()
+                inp = up + inp[3:]
+                return inp
+            inp = input("?!> ").strip()
+    
+    @staticmethod
+    def get_integer():
+        inp = input(">>> ").strip()
+        while True:
+            if inp.isdigit():
+                return inp
+            inp = input("?!> ").strip()
+
+    @staticmethod
+    def get_date():
+        inp = input(">>> ").strip()
+        while True:
+            spl = inp.split("-")
+            if len(spl) == 3 and all(map(lambda x: x.isdigit(), spl)):
+                return inp
+            inp = input("?!> ").strip()
+
+    @staticmethod
+    def get_group():
+        grp = Array()
+        grp.set(["F", "C", "O", "f", "c", "o"])
+        #
+        inp = input(">>> ").strip()
+        while True:
+            if len(inp) == 1 and inp in grp:
+                return inp.upper()
+            inp = input("?!> ").strip()
+
+    @staticmethod
+    def get_alpha():
+        inp = input(">>> ").strip()
+        while True:
+            if inp.isalpha():
+                return inp
+            inp = input("?!> ").strip()
+
+
